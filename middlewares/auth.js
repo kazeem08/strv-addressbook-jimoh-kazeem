8182308ef23c5af1a586d78b6940b7887346cb64
Terra-Kazeem <kjimoh@terragonltd.com>
2021-05-28 03:06:36 +0100

    code-refactor

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,6 @@ const config = require('../config/vars');
 //authentication to verify jwt token
 function auth(req, res, next) {
   const token = req.header('Authorization');
-  // if (!token) return res.status(401).send('Access denied! No token provided');
   if (!token) {
     return res.errorResponse({
       code: 400,
